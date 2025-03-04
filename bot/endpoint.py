3d9aedf8b7c8be9e8f6cc6c9d36ff58bbcd59c40
D3ENNY <denysraimondi06@gmail.com>
2025-03-04 19:00:24 +0100

    added put request for base64 data

--- bot/endpoint.py
+++ bot/endpoint.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+import base64, os
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +8,39 @@ def first_test():
     return jsonify({
         "message": "Hello World!"
     })
+
+@app.route('/api/upload_menu', methods=['POST'])
+def receive_menu():
+    try:
+        json = request.get_json()
+        
+        if not json: 
+            return jsonify({
+                "error": "No data sent to request"
+            }), 400
+
+        image_base64 = json.get("menu_base64")
+        if not image_base64:
+            return jsonify({
+                "error": "expected menu_base64 data but no data sent"
+            }), 400
+            
+        image_bytes = base64.b64decode(image_base64)
+        
+        #solo per test
+        with open(os.path.join("uploads", "output.png"), "wb") as output: 
+            output.write(image_bytes)
+        
+        return jsonify({
+            "message": "image succesfully received!", 
+            "file_path": os.path.join("uploads", "output.png")
+        })
+        
+        
+    except Exception as ex:
+        return jsonify({
+            "error": str(e)
+        }), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
